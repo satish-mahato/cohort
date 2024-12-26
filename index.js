4a60bdf62e11abc3a86da6203a8cce0e75143a39
Satish Kumar Mahato <84008484+satish-mahato@users.noreply.github.com>
2024-12-26 21:16:46 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-// const fs = require("fs");
-// function satish(cb) {
-//   fs.readFile("a.txt", "utf-8", (err, data) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//     cb(data);
-//   });
-// }
-// function onDone(data) {
-//   console.log(data);
-// }
-// satish(onDone);
-
-
-// by Promise
 const fs = require("fs");
-function satishPromise(){
-    return new Promise((resolve)=>{
-        fs.readFile("a.txt", "utf-8", (err, data)=>{
-            if (err){
-                console.log("error",err);
-            }
-            resolve(data);
-        });
-});
+function satish(cb) {
+  fs.readFile("a.txt", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    cb(data);
+  });
 }
-function onDone(data){
-    console.log(data);
+function onDone(data) {
+  console.log(data);
 }
-satishPromise().then(onDone);
+satish(onDone);
+
+
+
